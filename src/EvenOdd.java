9de10d49723550475727428ed8f63303a7b9dac0
Ashamango <ashaanupdas@gmail.com>
2023-06-03 21:24:37 -0400

    Displays if a number is even or odd

---
+++ src/EvenOdd.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class EvenOdd {
+    public static void main(String[] args) {
+        // Declare variables
+        int numToExamine = 100;
+        // If then statement
+        if (numToExamine % 2 == 0)
+            System.out.println("The number is an even number" );
+        else
+            System.out.println("The number is an odd number" );
+    }
+}
